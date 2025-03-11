f1d4b389d905c97b5b178dd6c4bc9e756372ea80
yanjies <48177787+yanjies@users.noreply.github.com>
2025-03-11 18:59:02 +0800

    Time: 12 ms (5.69%), Space: 20.8 MB (7.4%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,9 +1,8 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n=len(cost)
         @cache
         def dfs(i):
             if i<=1:
                 return 0
             return min(dfs(i-1)+cost[i-1],dfs(i-2)+cost[i-2])
-        return dfs(n)
+        return dfs(len(cost))
