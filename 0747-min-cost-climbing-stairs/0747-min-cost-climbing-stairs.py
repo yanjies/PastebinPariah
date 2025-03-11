13057c90909a9b6716117f92cb137f0a7dd423d8
yanjies <48177787+yanjies@users.noreply.github.com>
2025-03-11 18:59:19 +0800

    Time: 7 ms (21.89%), Space: 20.3 MB (7.4%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -4,5 +4,5 @@ class Solution:
         def dfs(i):
             if i<=1:
                 return 0
-            return min(dfs(i-1)+cost[i-1],dfs(i-2)+cost[i-2])
+            return min(dfs(i-2)+cost[i-2],dfs(i-1)+cost[i-1])
         return dfs(len(cost))
