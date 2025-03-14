06a2968e15c9480b70cb77c8a30a3a008d4a1429
yanjies <48177787+yanjies@users.noreply.github.com>
2025-03-14 16:37:00 +0800

    Time: 976 ms (41.77%), Space: 33.2 MB (45.89%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -3,14 +3,25 @@ class Solution:
         target=sum(nums)
         if target%2:
             return False
-        
         target//=2
-        @cache
-        def dfs(i,c):
-            if i<0:
-                return True if c==0 else False
-            if c<nums[i]:
-                return dfs(i-1,c)
-            return dfs(i-1,c) or dfs(i-1,c-nums[i])
+
+        # @cache
+        # def dfs(i,c):
+        #     if i<0:
+        #         return True if c==0 else False
+        #     if c<nums[i]:
+        #         return dfs(i-1,c)
+        #     return dfs(i-1,c) or dfs(i-1,c-nums[i])
         
-        return dfs(len(nums)-1,target)
+        # return dfs(len(nums)-1,target)
+
+        n=len(nums)
+        f=[[False]*(target+1) for _ in range(n+1)]
+        f[0][0]=True
+        for i,x in enumerate(nums):
+            for c in range(target+1):
+                if c<x:
+                    f[i+1][c]=f[i][c]
+                else:
+                    f[i+1][c]=f[i][c] or f[i][c-x]
+        return f[n][target]
