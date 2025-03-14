121bb7ce88be5840d4fd8ce1512934be8970c02e
yanjies <48177787+yanjies@users.noreply.github.com>
2025-03-14 16:42:52 +0800

    Time: 276 ms (53.61%), Space: 60 MB (36.47%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,11 +1,22 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        @cache
-        def dfs(i,c):
-            if i<0:
-                return 1 if c==0 else 0
-            if c<coins[i]:
-                return dfs(i-1,c)
-            return dfs(i-1,c)+dfs(i,c-coins[i])
+        # @cache
+        # def dfs(i,c):
+        #     if i<0:
+        #         return 1 if c==0 else 0
+        #     if c<coins[i]:
+        #         return dfs(i-1,c)
+        #     return dfs(i-1,c)+dfs(i,c-coins[i])
         
-        return dfs(len(coins)-1,amount)
+        # return dfs(len(coins)-1,amount)
+
+        n=len(coins)
+        f=[[0]*(amount+1) for _ in range(n+1)]
+        f[0][0]=1
+        for i,x in enumerate(coins):
+            for c in range(amount+1):
+                if c<x:
+                    f[i+1][c]=f[i][c]
+                else:
+                    f[i+1][c]=f[i][c]+f[i+1][c-x]
+        return f[n][amount]
